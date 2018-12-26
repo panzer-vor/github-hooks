df6c106c2dc3f1bd1093ad317d05662f9846ac16
panzer-vor <xyk0279@gmail.com>
2018-12-26 19:35:17 +0800

    test

--- tanpopo-github-hook/update-dev.sh
+++ tanpopo-github-hook/update-dev.sh
@@ -1,6 +1,9 @@
-echo "Start deployment"
-cd /var/www/tanpopo-dev/blog
-echo "pulling source code..."
-git clean -f
-git pull
-echo "Finished."
+# echo "Start deployment"
+# cd /var/www/tanpopo-dev/blog
+# echo "pulling source code..."
+# git clean -f
+# git pull
+# rm -rf /var/www/tanpopo-dev/admin
+# mv /var/www/tanpopo-dev/blog/admin/admin /var/www/tanpopo-dev
+# echo "Finished."
+echo "dev"
--- tanpopo-github-hook/update-master.sh
+++ tanpopo-github-hook/update-master.sh
@@ -1,6 +1,7 @@
-echo "Start deployment"
-cd /var/www/tanpopo/blog
-echo "pulling source code..."
-git clean -f
-git pull
-echo "Finished."
+# echo "Start deployment"
+# cd /var/www/tanpopo/blog
+# echo "pulling source code..."
+# git clean -f
+# git pull
+# echo "Finished."
+echo "pro"
