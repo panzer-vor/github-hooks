63272c8900dd89cc8900f355ed427adfb3f76a04
panzer-vor <xyk0279@gmail.com>
2018-12-26 22:18:59 +0800

    t

--- tanpopo-github-hook/index.js
+++ tanpopo-github-hook/index.js
@@ -1,8 +1,7 @@
 var spawn = require('child_process').spawn
 var http = require('http')
-var spawn = require('child_process').spawn
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/', secret: 'root'}) // 根据git上webhook的配置填写
+var handler = createHandler({ path: '/github-hook', secret: 'root'}) // 根据git上webhook的配置填写
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
@@ -20,7 +19,7 @@ handler.on('push', function (event) {
     event.payload.repository.name,
     event.payload.ref)
     init() // 每次拉取都重新监听
-}
+  }
 )   
 function rumCommand( cmd, args, cwd, callback ) {
   var child = spawn( cmd, args, {cwd: cwd} )
