2a8209d1fb789eb166cf61968b01e528d98b4e91
panzer-vor <xyk0279@gmail.com>
2018-12-27 11:48:02 +0800

    t

--- tanpopo-github-hook/update-dev.sh
+++ tanpopo-github-hook/update-dev.sh
@@ -1,9 +1,6 @@
-# echo "Start deployment"
-# cd /var/www/tanpopo-dev/blog
-# echo "pulling source code..."
-# git clean -f
-# git pull
-# rm -rf /var/www/tanpopo-dev/admin
-# mv /var/www/tanpopo-dev/blog/admin/admin /var/www/tanpopo-dev
-# echo "Finished."
-echo "dev"
+echo "Start deployment"
+cd /var/www/blog/test
+echo "pulling source code..."
+git clean -f
+git pull
+echo "Finished."
